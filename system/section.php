1f9fe042bcee39f8ec74b62f448e0f2cee836550
Arnold Huanca <jahvtrek@gmail.com>
2015-10-03 23:55:34 -0400

    Removiendo enlace innecesario

--- system/section.php
+++ system/section.php
@@ -12,7 +12,6 @@
     
     <div class="row">
     	<div class="col-xs-12">
-        
         	<div class="panel panel-primary">
             	<div class="panel-heading">
                 	<h3 class="panel-title">Gesti&oacute;n Ec&oacute;nomica</h3>
@@ -22,8 +21,8 @@
                 		<img class="img-responsive" src="../../img/services4.png">
                     </div>
                     <div class="media-body">
-                		<h3 class="media-heading"><a href="registrar_movimiento.php">Administraci&oacute;n Contable</a></h3>
-		              	<p>Manejo de dinero de las OTBs</p>
+                		<h3 class="media-heading">  Administraci&oacute;n Contable</h3>
+		              	<p>  Manejo de dinero de las OTBs</p>
 					</div>
                 </div>
             </div>
